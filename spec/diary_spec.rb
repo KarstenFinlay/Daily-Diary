fc286a1369fb0d53e5d51320f1370385efb684fa
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-28 11:29:02 +0000

    .all method now interacts with database

--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -4,12 +4,10 @@ describe Diary do
 
   describe '#.all' do
     it 'prints all saved bookmarks' do
-      diary = Diary.all
+      diary_entries = Diary.all
 
-      expect(diary).to include(
-        "I went on a walk today",
-        "I played Civ 6 for an hour today"
-        )
+      expect(diary_entries).to include "I went on a walk today"
+      expect(diary_entries).to include "I played Civ 6 for an hour today"
     end
   end
 
--- spec/features/view_diary_entries_spec.rb
+++ spec/features/view_diary_entries_spec.rb
@@ -1,6 +1,6 @@
 feature 'view_diary_entries' do
 
-  scenario 'There are preset diary entries' do
+  scenario 'Returns a lits of diary entries' do
     visit('/diary')
 
     expect(page).to have_content("I went on a walk today")
